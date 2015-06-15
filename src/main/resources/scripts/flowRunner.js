a17a53a5f471f51e5ff65441e2040d7f93e775e3
Michael <michael.alexandrov@gmail.com>
2015-06-15 18:47:35 +0300

    flowRunner functions

--- src/main/resources/scripts/flowRunner.js
+++ src/main/resources/scripts/flowRunner.js
@@ -1,17 +1,24 @@
 horny.load('scripts/lib/common.js');
 horny.load('scripts/lib/actionTypeToFunction.js');
 
-var flow = horny.readFile('flow/ttt/NewFlow.json');
 
-log('flow: ' + flow);
+var flowPath = System.getProperty("flowPath");
 
+runFlow(flowPath);
 
-flow = JSON.parse(flow);
+function runFlow(path){
+    var flow = horny.readFile(path);
+    log('flow: ' + flow);
 
-log('flow text: ' + flow.text);
+    flow = JSON.parse(flow);
+    log('flow text: ' + flow.text);
 
+    flow.steps.forEach(function(step){
+        runStep(step)
+    });
+}
 
-flow.steps.forEach(function(step){
+function runStep(step){
     log('step: ' + JSON.stringify(step));
 
     if(actionTypeToFunction[step.actionType]){
@@ -21,5 +28,4 @@ flow.steps.forEach(function(step){
     else{
         log('Unsupported action type - ' + step.actionType);
     }
-
-});
+}
