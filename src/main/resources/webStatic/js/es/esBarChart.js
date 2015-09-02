4b3416e43861b964881f58d04d1c88744b580a9c
Michael <michael.alexandrov@gmail.com>
2015-09-02 14:54:31 +0300

    add EsBarChart

---
+++ src/main/resources/webStatic/js/es/esBarChart.js
@@ -0,0 +1,12 @@
+
+Ext.define('Horny.EsBarChart', {
+    extend: 'Ext.chart.Chart',
+    alias: 'widget.esbarchart'
+
+    style: 'background:#fff',
+    animate: true,
+    shadow: true,
+
+
+
+});
